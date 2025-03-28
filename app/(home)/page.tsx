2b14dc6d2ae80ca07115b7c253922d6c4b107b4e
İbrahim <neodevils_contact@icloud.com>
2025-03-28 09:34:45 +0300

    Updated page.tsx & removed Main.tsx.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,43 @@
 import { Header } from '../components/Header';
-import { Main } from '../components/Main';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Main />
+      <main className={`${styles.main}`}>
+        <img
+          src='/assets/rogue.webp'
+          alt='Rogues'
+          className='photos'
+          loading='lazy'
+        />
+        <img
+          src='/assets/paladin.webp'
+          alt='Paladins'
+          className='photos'
+          loading='lazy'
+        />
+        <img
+          src='/assets/mage.webp'
+          alt='Mages'
+          className='photos'
+          loading='lazy'
+        />
+
+        <div>
+          <div className='video-mask'>
+            <h1>
+              <span className='sans-font'>Dungeon</span>
+              <br />
+              <span className='display-font'>Blitz</span>
+            </h1>
+          </div>
+          <h6>
+            A free-to-play, side-scrolling brawler with light RPG and MMO Game!
+          </h6>
+        </div>
+      </main>
     </>
   );
 }
